9c20b6f6939506b023ec67f2a05699a9e2c48727
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-06 23:14:12 +0600

    feat: add managed homepage SEO

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
 import HomeCms from "./components/homeCms";
+import { cmsMetadata } from "./lib/cms-metadata";
+
+export const generateMetadata = () => cmsMetadata(
+  "home",
+  "Home",
+  "Pilgrimage travel packages and guidance.",
+);
 
 export default function Home() {
   return <HomeCms />;
