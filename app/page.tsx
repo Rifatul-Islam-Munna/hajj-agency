c203bb74cc1f581e7ed0adf68b76d7e9af2a9e62
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-06 23:13:15 +0600

    feat: enable managed homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,5 @@
-import Header from "./layouts/header1";
-import Footer from "./layouts/footer";
-import Slider from "./components/slider";
-import About from "./components/about";
-import Pillars from "./components/pillars";
-import Services from "./components/services";
-import Packages from "./components/packages";
-import PrayerTime from "./components/prayerTime";
-import Scholars from "./components/scholars";
-import CounterUp from "./components/counterUp";
-import Testimonials from "./components/testimonials";
-import Blog from "./components/blog";
+import HomeCms from "./components/homeCms";
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <Slider />
-      <About />
-      <Pillars />
-      <Services />
-      <Packages />
-      <PrayerTime />
-      <Scholars />
-      <CounterUp />
-      <Testimonials />
-      <Blog />
-      <Footer />
-    </>
-  );
+  return <HomeCms />;
 }
