0bba2459acd730c260efb5045bfe22c72bfbbdbb
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-06 22:34:33 +0600

    refactor: redirect courses to packages

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -1,15 +1,2 @@
-import Header from "../layouts/header2";
-import Banner from '../components/banner';
-import Courses from '../template-parts/courses';
-import Footer from "../layouts/footer";
-
-export default function Home2() {
-  return (
-    <>
-    <Header/>
-    <Banner title="Courses" />
-    <Courses/>
-    <Footer/>
-    </>
-  )
-}
+import { redirect } from "next/navigation";
+export default function LegacyCoursesPage() { redirect("/packages"); }
