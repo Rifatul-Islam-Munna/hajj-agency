fac2ca154e03faa37e368160f966f1464d28620d
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-06 23:08:27 +0600

    feat: connect registration page to CMS

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,13 +1,23 @@
 import Header from "../layouts/header1";
-import Register from '../template-parts/register';
+import Register from "../template-parts/register";
 import Footer from "../layouts/footer";
+import { CmsPageProvider, CmsSection } from "../components/cmsPage";
+import { getCmsPage } from "../lib/cms-db";
+import { cmsMetadata } from "../lib/cms-metadata";
 
-export default function Home2() {
+export const generateMetadata = () => cmsMetadata(
+  "register",
+  "Register",
+  "Create your account.",
+);
+
+export default async function RegisterPage() {
+  const page = await getCmsPage("register").catch(() => null);
   return (
-    <>
-    <Header/>
-    <Register/>
-    <Footer/>
-    </>
-  )
+    <CmsPageProvider page={page}>
+      <Header />
+      <CmsSection sectionKey="register"><Register /></CmsSection>
+      <Footer />
+    </CmsPageProvider>
+  );
 }
