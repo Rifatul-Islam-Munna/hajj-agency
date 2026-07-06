a14e4fdfb8953717f626494a228da5a5c7660ce0
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-07 03:14:34 +0600

    feat: open blog create and edit modes

--- app/super-admin/blog/page.tsx
+++ app/super-admin/blog/page.tsx
@@ -1,6 +1,13 @@
-import { getBlogPosts } from "../../lib/blog-store";
+import { getBlogPostById, getBlogPosts } from "../../lib/blog-store";
+import BlogManager from "../blogManager";
 import PostList from "../postList";
 
-export default async function AdminBlogPage() {
+export default async function AdminBlogPage({ searchParams }: { searchParams: Promise<{ mode?: string; id?: string }> }) {
+  const params = await searchParams;
+  if (params.mode === "create") return <BlogManager initialPosts={[]} />;
+  if (params.mode === "edit" && Number(params.id)) {
+    const post = await getBlogPostById(Number(params.id));
+    return <BlogManager initialPosts={post ? [post] : []} />;
+  }
   return <PostList posts={await getBlogPosts({ enabledOnly: false })} />;
 }
