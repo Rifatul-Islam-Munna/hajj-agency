2e4fa6b2fe98b274595aafc483339e87f9737505
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-06 23:03:16 +0600

    feat: initialize CMS content system

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { Lexend } from "next/font/google";
-
-import ScrollToTop from './layouts/backToTop';
+import ScrollToTop from "./layouts/backToTop";
 import Preloader from "./layouts/preloader";
 import { initAuthDatabaseOnce } from "./lib/auth-db";
+import { initCmsDatabaseOnce } from "./lib/cms-db";
+import "./cms.css";
 
 const lexend = Lexend({
   subsets: ["latin"],
@@ -13,66 +14,33 @@ const lexend = Lexend({
 });
 
 export const metadata: Metadata = {
-  title: "Mihrab",
-  description: "Islamic Centre & Mosque Website",
+  title: { default: "Hajj and Umrah Agency", template: "%s | Hajj and Umrah Agency" },
+  description: "Trusted pilgrimage packages, guidance and travel support.",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   initAuthDatabaseOnce();
+  initCmsDatabaseOnce();
 
   return (
     <html lang="en" className={lexend.variable}>
       <head>
-        {/* Google Font Preconnect */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-
-        {/* Bootstrap */}
-        <link
-          rel="stylesheet"
-          href="/assets/vendor/bootstrap/css/bootstrap.min.css"
-        />
-
-        {/* Icons */}
-        <link
-          rel="stylesheet"
-          href="/assets/vendor/fontawesome/css/all.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="/assets/vendor/themify/themify.css"
-        />
-      {/* Animation */}
-       <link
-          rel="stylesheet"
-          href="/assets/css/animate.css"
-        />
-        {/* Boxicons */}
-        <link
-          href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
-          rel="stylesheet"
-        />
-        {/* phosphor-icons */}
-        <link
-        rel="stylesheet"
-        href="https://unpkg.com/@phosphor-icons/web@2.1.1/src/regular/style.css"
-      />
-
-        {/* Template CSS */}
+        <link rel="stylesheet" href="/assets/vendor/bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="/assets/vendor/fontawesome/css/all.min.css" />
+        <link rel="stylesheet" href="/assets/vendor/themify/themify.css" />
+        <link rel="stylesheet" href="/assets/css/animate.css" />
+        <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://unpkg.com/@phosphor-icons/web@2.1.1/src/regular/style.css" />
         <link rel="stylesheet" href="/assets/css/preloader.css" />
         <link rel="stylesheet" href="/assets/css/style.css" />
         <link rel="stylesheet" href="/assets/css/responsive.css" />
       </head>
-
       <body className="font-lexend">
-        <Preloader/>
+        <Preloader />
         {children}
         <ScrollToTop />
-  
       </body>
     </html>
   );
