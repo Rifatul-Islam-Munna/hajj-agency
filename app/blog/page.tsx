682d142386f38cb8a93131569a64b0458d6b9bc4
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-06 23:07:12 +0600

    feat: connect blog page to CMS

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,15 +1,25 @@
 import Header from "../layouts/header1";
-import Banner from '../components/banner';
-import Blog from '../template-parts/blog';
+import Banner from "../components/banner";
+import Blog from "../template-parts/blog";
 import Footer from "../layouts/footer";
+import { CmsPageProvider, CmsSection } from "../components/cmsPage";
+import { getCmsPage } from "../lib/cms-db";
+import { cmsMetadata } from "../lib/cms-metadata";
 
-export default function Home2() {
+export const generateMetadata = () => cmsMetadata(
+  "blog",
+  "Hajj and Umrah Blog",
+  "Travel tips and spiritual guidance.",
+);
+
+export default async function BlogPage() {
+  const page = await getCmsPage("blog").catch(() => null);
   return (
-    <>
-    <Header/>
-    <Banner title="Blog" />
-    <Blog/>
-    <Footer/>
-    </>
-  )
+    <CmsPageProvider page={page}>
+      <Header />
+      <CmsSection sectionKey="banner"><Banner title="Blog" /></CmsSection>
+      <CmsSection sectionKey="blog-grid"><Blog /></CmsSection>
+      <Footer />
+    </CmsPageProvider>
+  );
 }
