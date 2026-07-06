0417d4654a1cebeb8a7f2977ad2ba938c749af0e
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-06 22:57:23 +0600

    feat: add public packages page

---
+++ app/packages/page.tsx
@@ -0,0 +1,25 @@
+import Header from "../layouts/header2";
+import Banner from "../components/banner";
+import PackagesSection from "../components/packages";
+import Footer from "../layouts/footer";
+import { CmsPageProvider, CmsSection } from "../components/cmsPage";
+import { getCmsPage } from "../lib/cms-db";
+import { cmsMetadata } from "../lib/cms-metadata";
+
+export const generateMetadata = () => cmsMetadata(
+  "packages",
+  "Hajj and Umrah Packages",
+  "Browse our available pilgrimage travel packages.",
+);
+
+export default async function PackagesPage() {
+  const page = await getCmsPage("packages").catch(() => null);
+  return (
+    <CmsPageProvider page={page}>
+      <Header />
+      <CmsSection sectionKey="banner"><Banner title="Packages" /></CmsSection>
+      <CmsSection sectionKey="packages-grid"><PackagesSection featuredOnly={false} /></CmsSection>
+      <Footer />
+    </CmsPageProvider>
+  );
+}
