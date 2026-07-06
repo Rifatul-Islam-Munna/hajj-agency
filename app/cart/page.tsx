c08185ccbe16e7b6a3b57de53c921d0000f21998
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-07 03:00:08 +0600

    feat: use real package booking cart

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,25 +1,14 @@
 import Header from "../layouts/header2";
 import Banner from "../components/banner";
-import Cart from "../template-parts/cart";
+import BookingCartView from "../components/bookingCartView";
 import Footer from "../layouts/footer";
 import { CmsPageProvider, CmsSection } from "../components/cmsPage";
 import { getCmsPage } from "../lib/cms-db";
 import { cmsMetadata } from "../lib/cms-metadata";
 
-export const generateMetadata = () => cmsMetadata(
-  "cart",
-  "Booking Cart",
-  "Review your selected package.",
-);
+export const generateMetadata = () => cmsMetadata("cart", "Booking Cart", "Review your selected package.");
 
 export default async function CartPage() {
   const page = await getCmsPage("cart").catch(() => null);
-  return (
-    <CmsPageProvider page={page}>
-      <Header />
-      <CmsSection sectionKey="banner"><Banner title="Cart" /></CmsSection>
-      <CmsSection sectionKey="cart"><Cart /></CmsSection>
-      <Footer />
-    </CmsPageProvider>
-  );
+  return <CmsPageProvider page={page}><Header /><CmsSection sectionKey="banner"><Banner title="Booking Cart" /></CmsSection><CmsSection sectionKey="cart"><BookingCartView /></CmsSection><Footer /></CmsPageProvider>;
 }
