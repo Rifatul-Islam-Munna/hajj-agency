d04c15372b5de95464025f3058912ea1a87b6bf7
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-07 01:49:18 +0600

    feat: render dynamic Hajj blog posts

--- app/template-parts/blog.tsx
+++ app/template-parts/blog.tsx
@@ -1,220 +1,48 @@
-'use client';
+"use client";
+
 import { motion } from "framer-motion";
 import Link from "next/link";
+import type { BlogPost } from "../lib/cms-db";
 
-export default function BlogPage() {
+export default function BlogPage({ posts }: { posts: BlogPost[] }) {
   return (
     <section className="blog-page section-padding">
       <div className="container">
-        <div className="row">
-
-          {/* Blog Item 1 */}
-          <div className="col-xl-4 col-md-6 col-12">
-            <motion.div
-              className="single-blog"
-              initial={{ opacity: 0, y: 80 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{
-                duration: 0.8,
-                delay: 0.1,
-              }}
-              viewport={{ once: true }}
-            >
-              <div className="blog-image position-relative">
-                <img src="/assets/img/blog/1.jpg" alt="Blog Image"/>
-                <div className="blog_category">
-                  <Link href="#">Quran Learning</Link>
+        <div className="row g-4">
+          {posts.map((item, index) => (
+            <div className="col-xl-4 col-md-6 col-12" key={item.id}>
+              <motion.div
+                className="single-blog"
+                initial={{ opacity: 0, y: 80 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8, delay: Math.min(index, 5) * 0.06 }}
+                viewport={{ once: true }}
+              >
+                <div className="blog-image position-relative">
+                  <img src={item.featured_image || "/assets/img/blog/1.jpg"} alt={item.title} />
+                  <div className="blog_category"><Link href={`/blog/${item.slug}`}>{item.category}</Link></div>
                 </div>
-              </div>
-
-              <div className="blog_content">
-                <div className="blog_meta d-flex justify-content-between">
-                  <span><i className="fa-regular fa-clock"></i> 18 August, 2026</span>
-                  <span><i className="fa-regular fa-comment"></i> <Link href="#">25 Comments</Link></span>
+                <div className="blog_content">
+                  <div className="blog_meta d-flex justify-content-between">
+                    <span><i className="fa-regular fa-clock"></i> {formatDate(item.published_at)}</span>
+                    <span><i className="fa-regular fa-user"></i> {item.author_name}</span>
+                  </div>
+                  <h3><Link href={`/blog/${item.slug}`}>{item.title}</Link></h3>
+                  <Link href={`/blog/${item.slug}`} className="blog_btn">Read More</Link>
                 </div>
-                <h3>
-                  <Link href="/blog-details">What Are The Conditions Of The Shahadah In Islam</Link>
-                </h3>
-                <Link href="/blog-details" className="blog_btn">Read More</Link>
-              </div>
-            </motion.div>
-          </div>
-
-          {/* Blog Item 2 */}
-          <div className="col-xl-4 col-md-6 col-12">
-            <motion.div
-              className="single-blog"
-              initial={{ opacity: 0, y: 80 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{
-                duration: 0.8,
-                delay: 0.1,
-              }}
-              viewport={{ once: true }}
-            >
-              <div className="blog-image position-relative">
-                <img src="/assets/img/blog/2.jpg" alt="Blog Image"  />
-                <div className="blog_category">
-                  <Link href="#">Prayer</Link>
-                </div>
-              </div>
-
-              <div className="blog_content">
-                <div className="blog_meta d-flex justify-content-between">
-                  <span><i className="fa-regular fa-clock"></i> 18 August, 2026</span>
-                  <span><i className="fa-regular fa-comment"></i> <Link href="#">25 Comments</Link></span>
-                </div>
-                <h3>
-                  <Link href="/blog-details">Abandoning Prayer – Why To Avoid At All Costs?</Link>
-                </h3>
-                <Link href="/blog-details" className="blog_btn">Read More</Link>
-              </div>
-            </motion.div>
-          </div>
-
-          {/* Blog Item 3 */}
-          <div className="col-xl-4 col-md-6 col-12">
-            <motion.div
-              className="single-blog"
-              initial={{ opacity: 0, y: 80 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{
-                duration: 0.8,
-                delay: 0.1,
-              }}
-              viewport={{ once: true }}
-            >
-              <div className="blog-image position-relative">
-                <img src="/assets/img/blog/3.jpg" alt="Blog Image"  />
-                <div className="blog_category">
-                  <Link href="#">Islamic</Link>
-                </div>
-              </div>
-
-              <div className="blog_content">
-                <div className="blog_meta d-flex justify-content-between">
-                  <span><i className="fa-regular fa-clock"></i> 18 August, 2026</span>
-                  <span><i className="fa-regular fa-comment"></i> <Link href="#">25 Comments</Link></span>
-                </div>
-                <h3>
-                  <Link href="/blog-details">The Purpose Of Revelation Of The Holy Quran</Link>
-                </h3>
-                <Link href="/blog-details" className="blog_btn">Read More</Link>
-              </div>
-            </motion.div>
-          </div>
-
-          {/* Blog Item 4 */}
-          <div className="col-xl-4 col-md-6 col-12">
-            <motion.div
-              className="single-blog"
-              initial={{ opacity: 0, y: 80 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{
-                duration: 0.8,
-                delay: 0.1,
-              }}
-              viewport={{ once: true }}
-            >
-              <div className="blog-image position-relative">
-                <img src="/assets/img/blog/3.jpg" alt="Blog Image"/>
-                <div className="blog_category">
-                  <Link href="#">Quran Learning</Link>
-                </div>
-              </div>
-
-              <div className="blog_content">
-                <div className="blog_meta d-flex justify-content-between">
-                  <span><i className="fa-regular fa-clock"></i> 18 August, 2026</span>
-                  <span><i className="fa-regular fa-comment"></i> <Link href="#">25 Comments</Link></span>
-                </div>
-                <h3>
-                  <Link href="/blog-details">The Purpose Of Revelation Of The Holy Quran</Link>
-                </h3>
-                <Link href="/blog-details" className="blog_btn">Read More</Link>
-              </div>
-            </motion.div>
-          </div>
-
-          {/* Blog Item 5 */}
-          <div className="col-xl-4 col-md-6 col-12">
-            <motion.div
-              className="single-blog"
-              initial={{ opacity: 0, y: 80 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{
-                duration: 0.8,
-                delay: 0.1,
-              }}
-              viewport={{ once: true }}
-            >
-              <div className="blog-image position-relative">
-                <img src="/assets/img/blog/1.jpg" alt="Blog Image" />
-                <div className="blog_category">
-                  <Link href="#">Prayer</Link>
-                </div>
-              </div>
-
-              <div className="blog_content">
-                <div className="blog_meta d-flex justify-content-between">
-                  <span><i className="fa-regular fa-clock"></i> 18 August, 2026</span>
-                  <span><i className="fa-regular fa-comment"></i> <Link href="#">25 Comments</Link></span>
-                </div>
-                <h3>
-                  <Link href="/blog-details">What Are The Conditions Of The Shahadah In Islam</Link>
-                </h3>
-                <Link href="/blog-details" className="blog_btn">Read More</Link>
-              </div>
-            </motion.div>
-          </div>
-
-          {/* Blog Item 6 */}
-          <div className="col-xl-4 col-md-6 col-12">
-            <motion.div
-              className="single-blog"
-              initial={{ opacity: 0, y: 80 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{
-                duration: 0.8,
-                delay: 0.1,
-              }}
-              viewport={{ once: true }}
-            >
-              <div className="blog-image position-relative">
-                <img src="/assets/img/blog/2.jpg" alt="Blog Image" />
-                <div className="blog_category">
-                  <Link href="#">Islamic</Link>
-                </div>
-              </div>
-
-              <div className="blog_content">
-                <div className="blog_meta d-flex justify-content-between">
-                  <span><i className="fa-regular fa-clock"></i> 18 August, 2026</span>
-                  <span><i className="fa-regular fa-comment"></i> <Link href="#">25 Comments</Link></span>
-                </div>
-                <h3>
-                  <Link href="/blog-details">Abandoning Prayer – Why To Avoid At All Costs?</Link>
-                </h3>
-                <Link href="/blog-details" className="blog_btn">Read More</Link>
-              </div>
-            </motion.div>
-          </div>
-
-          {/* Pagination */}
-          <div className="col-12 text-center wow fadeInUp">
-            <div className="post_pagination">
-              <ul>
-                <li><Link href="#"><i className="ph ph-arrow-left"></i></Link></li>
-                <li><Link href="#">1</Link></li>
-                <li className="active"><Link href="#">2</Link></li>
-                <li><Link href="#">3</Link></li>
-                <li><Link href="#"><i className="ph ph-arrow-right"></i></Link></li>
-              </ul>
+              </motion.div>
             </div>
-          </div>
-
+          ))}
+          {posts.length === 0 && <div className="col-12 text-center"><p>No blog posts have been published yet.</p></div>}
         </div>
       </div>
     </section>
   );
+}
+
+function formatDate(value: string) {
+  const date = new Date(`${value}T00:00:00`);
+  return Number.isNaN(date.getTime())
+    ? value
+    : new Intl.DateTimeFormat("en-GB", { day: "numeric", month: "long", year: "numeric" }).format(date);
 }
