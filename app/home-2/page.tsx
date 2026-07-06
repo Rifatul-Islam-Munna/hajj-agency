876f73319d499f4458a2b4c12f7472f80d6768df
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-06 23:14:24 +0600

    feat: add managed second homepage SEO

--- app/home-2/page.tsx
+++ app/home-2/page.tsx
@@ -1,4 +1,11 @@
 import HomeTwoCms from "../components/homeTwoCms";
+import { cmsMetadata } from "../lib/cms-metadata";
+
+export const generateMetadata = () => cmsMetadata(
+  "home-2",
+  "Home",
+  "Plan your pilgrimage journey with trusted travel specialists.",
+);
 
 export default function HomeTwo() {
   return <HomeTwoCms />;
