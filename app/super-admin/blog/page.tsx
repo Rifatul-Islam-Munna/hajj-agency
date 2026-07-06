1d2d90c83af065d9ed469d7f297e3d34061ae5cf
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-07 03:12:39 +0600

    feat: use table blog list

--- app/super-admin/blog/page.tsx
+++ app/super-admin/blog/page.tsx
@@ -1,7 +1,6 @@
 import { getBlogPosts } from "../../lib/blog-store";
-import BlogManager from "../blogManager";
+import PostList from "../postList";
 
 export default async function AdminBlogPage() {
-  const posts = await getBlogPosts({ enabledOnly: false });
-  return <BlogManager initialPosts={posts} />;
+  return <PostList posts={await getBlogPosts({ enabledOnly: false })} />;
 }
