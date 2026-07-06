b712048bc9cbfbd9bdf916ae1b5cb11f1bb773ae
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-07 03:17:26 +0600

    feat: add package category links

--- app/packages/page.tsx
+++ app/packages/page.tsx
@@ -1,25 +1,15 @@
 import Header from "../layouts/header2";
 import Banner from "../components/banner";
 import PackagesSection from "../components/packages";
+import PackageCategoryNav from "../components/packageCategoryNav";
 import Footer from "../layouts/footer";
 import { CmsPageProvider, CmsSection } from "../components/cmsPage";
 import { getCmsPage } from "../lib/cms-db";
 import { cmsMetadata } from "../lib/cms-metadata";
 
-export const generateMetadata = () => cmsMetadata(
-  "packages",
-  "Hajj and Umrah Packages",
-  "Browse our available pilgrimage travel packages.",
-);
+export const generateMetadata = () => cmsMetadata("packages", "Hajj and Umrah Packages", "Browse our available pilgrimage travel packages.");
 
 export default async function PackagesPage() {
   const page = await getCmsPage("packages").catch(() => null);
-  return (
-    <CmsPageProvider page={page}>
-      <Header />
-      <CmsSection sectionKey="banner"><Banner title="Packages" /></CmsSection>
-      <CmsSection sectionKey="packages-grid"><PackagesSection featuredOnly={false} /></CmsSection>
-      <Footer />
-    </CmsPageProvider>
-  );
+  return <CmsPageProvider page={page}><Header /><CmsSection sectionKey="banner"><Banner title="Packages" /></CmsSection><PackageCategoryNav /><CmsSection sectionKey="packages-grid"><PackagesSection featuredOnly={false} /></CmsSection><Footer /></CmsPageProvider>;
 }
