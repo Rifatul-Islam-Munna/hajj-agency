07ac80590dbcec1cc5b8c1663347da34730e2237
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-06 22:36:02 +0600

    feat: connect login page content controls

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,19 @@
 import Header from "../layouts/header1";
 import Login from "../template-parts/login";
 import Footer from "../layouts/footer";
+import { CmsPageProvider, CmsSection } from "../components/cmsPage";
+import { getCmsPage } from "../lib/cms-db";
+import { cmsMetadata } from "../lib/cms-metadata";
 
-export default function LoginPage() {
+export const generateMetadata = () => cmsMetadata("login", "Login", "Sign in to your account.");
+
+export default async function LoginPage() {
+  const page = await getCmsPage("login").catch(() => null);
   return (
-    <>
+    <CmsPageProvider page={page}>
       <Header />
-      <Login />
+      <CmsSection sectionKey="login"><Login /></CmsSection>
       <Footer />
-    </>
+    </CmsPageProvider>
   );
 }
