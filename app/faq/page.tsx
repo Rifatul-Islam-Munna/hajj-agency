f2e5770bc18c8c82e4c0300f4b4d58fccf2af234
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-06 23:06:33 +0600

    feat: connect FAQ page to CMS

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,15 +1,25 @@
 import Header from "../layouts/header1";
-import Banner from '../components/banner';
-import Faq from '../components/faq';
+import Banner from "../components/banner";
+import Faq from "../components/faq";
 import Footer from "../layouts/footer";
+import { CmsPageProvider, CmsSection } from "../components/cmsPage";
+import { getCmsPage } from "../lib/cms-db";
+import { cmsMetadata } from "../lib/cms-metadata";
 
-export default function Home2() {
+export const generateMetadata = () => cmsMetadata(
+  "faq",
+  "Frequently Asked Questions",
+  "Answers to common pilgrimage travel questions.",
+);
+
+export default async function FaqPage() {
+  const page = await getCmsPage("faq").catch(() => null);
   return (
-    <>
-    <Header/>
-    <Banner title="Contact Us" />
-    <Faq/>
-    <Footer/>
-    </>
-  )
+    <CmsPageProvider page={page}>
+      <Header />
+      <CmsSection sectionKey="banner"><Banner title="Frequently Asked Questions" /></CmsSection>
+      <CmsSection sectionKey="faq"><Faq /></CmsSection>
+      <Footer />
+    </CmsPageProvider>
+  );
 }
