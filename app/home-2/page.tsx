e5ef8f9e3e5b41b40cd9eae3b03a11a4be529b64
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-07 03:10:55 +0600

    chore: redirect unused home banner route

--- app/home-2/page.tsx
+++ app/home-2/page.tsx
@@ -1,12 +1,2 @@
-import HomeTwoCms from "../components/homeTwoCms";
-import { cmsMetadata } from "../lib/cms-metadata";
-
-export const generateMetadata = () => cmsMetadata(
-  "home-2",
-  "Home",
-  "Plan your pilgrimage journey with trusted travel specialists.",
-);
-
-export default function HomeTwo() {
-  return <HomeTwoCms />;
-}
+import { redirect } from "next/navigation";
+export default function HomeTwo() { redirect("/"); }
