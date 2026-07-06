8729a127d268df38eef694e5533598b83c1acf98
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-06 23:07:40 +0600

    feat: connect cart page to CMS

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,15 +1,25 @@
 import Header from "../layouts/header2";
-import Banner from '../components/banner';
-import Cart from '../template-parts/cart';
+import Banner from "../components/banner";
+import Cart from "../template-parts/cart";
 import Footer from "../layouts/footer";
+import { CmsPageProvider, CmsSection } from "../components/cmsPage";
+import { getCmsPage } from "../lib/cms-db";
+import { cmsMetadata } from "../lib/cms-metadata";
 
-export default function Home2() {
+export const generateMetadata = () => cmsMetadata(
+  "cart",
+  "Booking Cart",
+  "Review your selected package.",
+);
+
+export default async function CartPage() {
+  const page = await getCmsPage("cart").catch(() => null);
   return (
-    <>
-    <Header/>
-    <Banner title="Cart" />
-    <Cart/>
-    <Footer/>
-    </>
-  )
+    <CmsPageProvider page={page}>
+      <Header />
+      <CmsSection sectionKey="banner"><Banner title="Cart" /></CmsSection>
+      <CmsSection sectionKey="cart"><Cart /></CmsSection>
+      <Footer />
+    </CmsPageProvider>
+  );
 }
