a75e443900f42147d75a60dde5b72962126faa3f
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-06 23:07:50 +0600

    feat: connect checkout page to CMS

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,15 +1,25 @@
 import Header from "../layouts/header2";
-import Banner from '../components/banner';
-import Checkout from '../template-parts/checkout';
+import Banner from "../components/banner";
+import Checkout from "../template-parts/checkout";
 import Footer from "../layouts/footer";
+import { CmsPageProvider, CmsSection } from "../components/cmsPage";
+import { getCmsPage } from "../lib/cms-db";
+import { cmsMetadata } from "../lib/cms-metadata";
 
-export default function Home2() {
+export const generateMetadata = () => cmsMetadata(
+  "checkout",
+  "Booking Checkout",
+  "Complete your package booking request.",
+);
+
+export default async function CheckoutPage() {
+  const page = await getCmsPage("checkout").catch(() => null);
   return (
-    <>
-    <Header/>
-    <Banner title="Checkout" />
-    <Checkout/>
-    <Footer/>
-    </>
-  )
+    <CmsPageProvider page={page}>
+      <Header />
+      <CmsSection sectionKey="banner"><Banner title="Checkout" /></CmsSection>
+      <CmsSection sectionKey="checkout"><Checkout /></CmsSection>
+      <Footer />
+    </CmsPageProvider>
+  );
 }
